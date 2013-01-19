bc9e3edf396776b9ac5eb82a44662ce95af59db9
tryggve <tryggve@tryggve.nu>
2013-01-19 15:44:40 +0100

    Adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyLocalFoodStore::Application.config.secret_token = '5d19a0c42cd2e5293e59c3680a06844914141cd0350459609b85f59e91aa059fdfddb40a723250b7a125fd0f076c1d46ef8eb82c7a1cc127ab08aeb37b6ddfe4'
